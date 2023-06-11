fdb2fef8dec92bce617e6f233e69fb633db37446
Rajiv kumar <rajivkumarbna@gmail.com>
2023-06-11 13:15:54 +0530

    Impl

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Singleton firstInstance = Singleton.getInstance();
+        Singleton secondInstance = Singleton.getInstance();
+
+    }
+}
